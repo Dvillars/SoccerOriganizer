874ab2d72713b0580b86d76d18dfe68267037aca
Derek Villars and Elise St Hilaire <derek+elise@>
2017-04-18 11:47:17 -0700

    add inital commit...

---
+++ src/SoccerOrganizer/Controllers/DivisionsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoccerOrganizer.Models;
+
+
+
+namespace SoccerOrganizer.Models.Controllers
+{
+    public class DivisionsController : Controller
+    {
+        private SoccerOrganizerContext db = new SoccerOrganizerContext();
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Division division)
+        {
+            db.Divisions.Add(division);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/SoccerOrganizer/Models/SoccerOrganizerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoccerOrganizer.Models
+{
+    public class SoccerOrganizerContext: DbContext
+    {
+        public virtual DbSet<Division> Divisions { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SoccerOrganizer;integrated security=True");
+        }
+    }
+}
